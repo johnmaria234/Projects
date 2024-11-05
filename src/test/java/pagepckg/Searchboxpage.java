002fd5799802681b49de3d8516e486ca75f703e4
Maria John <johnmaria234@gmail.com>
2024-11-05 17:19:32 +0530

    basic tests

--- src/test/java/pagepckg/Searchboxpage.java
+++ src/test/java/pagepckg/Searchboxpage.java
@@ -1,7 +1,4 @@
 package pagepckg;
-
-
-
 import java.time.Duration;
 import java.util.List;
 import org.openqa.selenium.JavascriptExecutor;
@@ -16,12 +13,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class Searchboxpage {
-	     WebDriver driver;
-	   
-	     @FindBy(xpath="//*[@id=\"home-react-root\"]/main/div/div[1]/div/div[1]/button")
-			WebElement popup;
-	  
-	//SEARCHBOX
+	     WebDriver driver;	   
+	    @FindBy(xpath="//*[@id=\"home-react-root\"]/main/div/div[1]/div/div[1]/button")
+		WebElement popup;	  
+	   //SEARCHBOX
 		@FindBy(xpath="//*[@id='tab-all-rooms-tab']/div/div")
 		WebElement Hotelstab;
 		@FindBy(xpath="//*[@id='tab-home']/div/div")
@@ -51,26 +46,50 @@ public class Searchboxpage {
 		@FindBy(xpath="//*[@id=\"SearchBoxContainer\"]/div[2]")
 		WebElement Searchbutton;
 		
-		//searchactivities
+	
+		
+		//HOMES AND APARTMNTS SEARCHBAR
+		//FLIGTHS AND HOTELS SEARCHBAR
+		//AIRPORTTRANSFER  SEARCHBAR
+		
+		//FLIGTHS  SEARCHBAR
+		@FindBy(xpath="//*[@id=\"tabpanel-flight-tab\"]/div[1]/div/div[1]/div/div[1]/div[1]/div/div/div[1]/button")
+		WebElement oneway;
+		@FindBy(xpath="//*[@id=\"tabpanel-flight-tab\"]/div[1]/div/div[1]/div/div[1]/div[1]/div/div/div[2]/button")
+		WebElement roundtrip;
+		@FindBy(xpath="//*[@id=\"flight-origin-text-search\"]/div")
+		WebElement flyingfrom;
+		@FindBy(xpath="//*[@id=\"flight-destination-search-input\"]")
+		WebElement flyingto;
+		@FindBy(xpath="//*[@id=\"flight-departure\"]/div/div/div/div")
+		WebElement departure;
+		@FindBy(xpath=" ")
+		WebElement arrival;
+		@FindBy(xpath="//*[@id=\"flight-occupancy\"]/div/div/div")
+		WebElement passangereconomy;
+		@FindBy(xpath="//*[@id=\"Tabs-Container\"]/button/div")
+		WebElement searchbarf;
+		
+		
+		//ACTIVITIES  SEARCHBAR
 		@FindBy(xpath="//*[@id=\"Tabs-Container\"]/button")
 		WebElement Searchbuttonact;
 		@FindBy(xpath="//*[@id=\"activities-search-input\"]")
 		WebElement activitiessearch;
+		@FindBy(xpath="//*[@id=\"autocompleteSearch\"]/div/div/ul/li[1]")
+		WebElement searchsuggestionact;
+		
+	  
 		
 		
-		//HOMES AND APARTMNTS SEARCHBAR
-		//FLIGTHS AND HOTELS SEARCHBAR
-		//FLIGTHS  SEARCHBAR
-		//ACTIVITIES  SEARCHBAR
-		//AIRPORTTRANSFER  SEARCHBAR
-		
-	//Search bar
+	  //SearchBar
 		@FindBy(xpath="//*[@id=\"textInput\"]")
 		 WebElement search;
 		@FindBy(xpath="//*[@id=\"home-react-root\"]/main/section/section")
 		 WebElement clickoutofbox;
 		@FindBy(xpath="//div[@id='SearchBoxContainer']/div/div/div[2]/div/div/div[6]/div/div/ul/li")
 		WebElement  searchlist;
+		
 
 	//DATEPICKER
 		@FindBy(xpath="//div[@id='DatePicker__Accessible']/div/div[2]/div/div")
@@ -80,7 +99,7 @@ public class Searchboxpage {
 		@FindBy(xpath="//div[@id=\"DatePicker__Accessible\"]/div/div[2]/div[1]/div[3]/div/div")
 		 List<WebElement> datelist;
 
-		//numberselectbutton
+		//NumberSelectbutton
 		@FindBy(xpath="//*[@id=\"FocusTrap\"]/div/div[1]/div[1]/div[2]/div[2]/div/button")
 		WebElement room;
 		@FindBy(xpath="//*[@id=\"FocusTrap\"]/div/div/div[2]/div[2]/button[2]")
@@ -92,57 +111,45 @@ public class Searchboxpage {
 		
 		public  Searchboxpage(WebDriver driver) {		
 			this.driver = driver;
-			PageFactory.initElements(driver, this);
-			                             	}
+			PageFactory.initElements(driver, this);	               	}
 		
 		
 		public void popupclose() {
 			WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
 			wait.until(ExpectedConditions.elementToBeClickable(popup));
-			popup.click();
-		}
+			popup.click();		}
 		
 		
-		public void hotels()  {
-	
-			Hotelstab.click(); }
+		public void hotels()  { Hotelstab.click(); }
 		
 		public void hotelssearchbar(String place) {
-            	search.sendKeys(place);
-                  clickoutofbox.click();
-                  
-//			Actions act = new Actions(driver);
-//			act.sendKeys(Keys.ARROW_DOWN,Keys.ENTER);                 
-//			act.perform(); 
-		}
+            search.sendKeys(place);
+  			      Actions act = new Actions(driver);
+             act.keyDown(searchlist,Keys.ARROW_DOWN).keyUp(Keys.ARROW_DOWN); 
+             act.keyDown(Keys.ENTER).keyUp(Keys.ENTER); 
+             act.perform(); 	}
 		
 		public void hotelscheckin(String expmonth,String expdate) {
-			         Checkin.click();
-                     datepicker(expmonth,expdate);  }
+              datepicker(expmonth,expdate);  }
 		public void hotelscheckout(String expmonth,String expdate) {
-                 datepicker(expmonth,expdate);  }
+              datepicker(expmonth,expdate);  }
 		
 		public void slots(int rn,int pn,int cn) {	
-			WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
-		
-			 ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 0)");
-			          while(rn>1) { room.click();rn--; }
-		              while(pn>2) { person.click(); pn--; }
-		           
-		          	wait.until(ExpectedConditions.elementToBeClickable(children));
-		              while(cn>0) { 
-		            	  children.click(); cn--;}  
-		          	wait.until(ExpectedConditions.elementToBeClickable(Noofpersonandrooms));
-		          	Noofpersonandrooms.click();
-		          	}
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));	
+		((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 0)");
+			  while(rn>1) { room.click();rn--; }
+		      while(pn>2) { person.click(); pn--; }	           
+		      wait.until(ExpectedConditions.elementToBeClickable(children));
+		      while(cn>0) { 
+		      children.click(); cn--;}  
+		       wait.until(ExpectedConditions.elementToBeClickable(Noofpersonandrooms));
+		       Noofpersonandrooms.click();   	}
 			   
 		public void searchbutton() { Searchbutton.click(); }
 		
-		public void dayusehotel(String expmonth,String expdate) {Dayusestays.click();
-			Checkin.click();
-			datepicker(expmonth,expdate);
-		
-		}
+		public void dayusehotel(String expmonth,String expdate) {  Dayusestays.click();
+			   Checkin.click();
+			   datepicker(expmonth,expdate);    }
 
 		public  void datepicker(String expmonth,String expdate) {
 			WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
@@ -150,29 +157,31 @@ public class Searchboxpage {
 			    String actualmonth = monthdetails.getText();
 			    if(actualmonth.equalsIgnoreCase(expmonth)) {System.out.println(actualmonth);break; }
 			    else {   wait.until(ExpectedConditions.elementToBeClickable(nextbutton));
-			    	nextbutton.click(); }        }    
-			     
+			    nextbutton.click(); }        }        
 			   for(WebElement ele:datelist) {			   
 				String actdate =ele.getText();
-				if(actdate.equals(expdate)) {ele.click();break;} }
-			         
-			}
+				if(actdate.equals(expdate)) {ele.click();break;} }  	}
 		
 		
-	public void activities(String placeoractivity) {
-		Activitiestab.click();
-		activitiessearch.sendKeys(placeoractivity,Keys.ARROW_DOWN,Keys.ENTER);
-
-//		Actions act = new Actions(driver);
-//		act.sendKeys(Keys.ARROW_DOWN,Keys.ENTER);                 
-//		act.perform(); 
-		Searchbuttonact.click();
-	    }	
+	     public void activities(String placeoractivity) {
+		   Activitiestab.click();
+		   activitiessearch.sendKeys(placeoractivity);
+		   Actions act = new Actions(driver);
+           act.keyDown(searchsuggestionact,Keys.ARROW_DOWN).keyUp(Keys.ARROW_DOWN); 
+           act.keyDown(Keys.ENTER).keyUp(Keys.ENTER); 
+           act.perform(); 
+           Searchbuttonact.click();    }
+	     
+	     
+	     public void flights() {
+	    	 Flightstab.click();
+	    	 
+	    	 
+	     }
+	     
 	
 	
-		
-		
-		}
+}
 		
 
 
--- src/test/java/testpackg/SearchboxTest.java
+++ src/test/java/testpackg/SearchboxTest.java
@@ -1,14 +1,8 @@
 package testpackg;
-
-
 import java.io.IOException;
-
 import java.time.Duration;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-
 import basepackg.Baseclass;
 import pagepckg.Searchboxpage;
 
@@ -20,15 +14,14 @@ public class SearchboxTest extends Baseclass {
     public void Test1hotel() throws IOException  {
     	test = extent.createTest("Test1hotel");
     	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
- 	   Searchboxpage ob= new Searchboxpage(driver);
+ 	    Searchboxpage ob= new Searchboxpage(driver);
  	         ob.popupclose();
- 	               ob.hotels();
- 	              // ob.hotelssearchbar("Munnar");
+ 	              ob.hotels();
+ 	               ob.hotelssearchbar("Munnar");
  	               ob.hotelscheckin("December 2024", "9");
  	               ob.hotelscheckout("December 2024", "12");
  	               ob.slots(2,4,1);
  	               ob.searchbutton();
-
  	               Assert.assertEquals((driver.getCurrentUrl()), "https://www.agoda.com/en-in/activities/search?cityId");
  	                               }
     
@@ -37,20 +30,19 @@ public class SearchboxTest extends Baseclass {
     public void Test2() throws IOException  { 
     	test = extent.createTest("Test2hotel");
     	driver.navigate().refresh();
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-    	   Searchboxpage ob= new Searchboxpage(driver);
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    	Searchboxpage ob= new Searchboxpage(driver);
     	      ob.hotels();
-    //        ob.hotelssearchbar("Munnar");
+              ob.hotelssearchbar("Munnar");
     	      ob.dayusehotel("January 2025", "25");
     	      ob.slots(1, 2, 1);
     	      ob.searchbutton();
     	      Assert.assertEquals((driver.getCurrentUrl()), "https://www.agoda.com/en-in/activities/search?cityId");
     }
     
-    @Test
+    @Test(enabled=true)
     public void Test3() {
       	test = extent.createTest("Test3Activities");
-    	driver.navigate().refresh();
 	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     	Searchboxpage ob= new Searchboxpage(driver);
     	ob.activities("Delhi");
